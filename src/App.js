ed26283ed41f3327f440b49dc5a9229df364cb85
pkshort <pshort@ithaca.edu>
2019-02-25 22:03:48 -0500

    Creating API link

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const instanceLocator = ""
+
+const testToken = ""
+const username = ""
+const roomId = 0
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      messages: DUMMY_DATA
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+      <Title />
+      <MessageList messages={this.state.messages}/>
+      <SendMessageForm />
       </div>
-    );
+    )
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: username,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    })
+
+    chatManager.connect().then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+          onNewMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
+        }
+      })
+    })
   }
 }
+
+const DUMMY_DATA = [
+  {
+    senderId: "John",
+    text: "What's the score?"
+  },
+  {
+    senderId: "Jane",
+    text: "Who's playing?"
+  }
+]
+
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <ul className="message-list">
+      {this.props.messages.map(message => {
+        return (
+          <li key={message.id}>
+            <div>
+              {message.senderId}
+            </div>
+            <div>
+              {message.text}
+            </div>
+            </li>
+        )
+      })}
+      </ul>
+    )
+  }
+}
+
 
 export default App;
